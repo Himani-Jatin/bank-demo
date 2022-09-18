c4dc6b78adf2267c587021d6cd71cf2464df4c39
Himani-Jatin <geetika.jatin.himani@gmail.com>
2022-09-18 15:09:33 +0530

    connected with mongodb

--- bankdemo/settings.py
+++ bankdemo/settings.py
@@ -78,10 +78,17 @@ WSGI_APPLICATION = 'bankdemo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'bankdemo',
     }
 }
 
